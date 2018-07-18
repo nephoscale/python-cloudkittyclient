7868cbe17d7cad8bdb9c58e43cd1b7d9d8451aef
deepthy <deepthy@poornam.com>
2018-07-18 19:29:35 +0530

    customisations integrated to pike version

--- cloudkittyclient/v1/report/__init__.py
+++ cloudkittyclient/v1/report/__init__.py
@@ -30,16 +30,27 @@ class ReportSummary(base.Resource):
     def __repr__(self):
         return "<Summary %s" % self._info
 
+class ReportResult(base.Resource):
+
+    key = 'report'
+
+    def __repr__(self):
+        return "<Report %s>" % self._info
 
 class ReportManager(base.CrudManager):
 
     base_url = "/v1/report"
+    resource_class = ReportResult
+    key = "report"
+    collection_key = "reports"
 
     def list_tenants(self):
         return self.client.get(self.base_url + "/tenants").json()
 
+    # Modified by Muralidharan.s for applying a logic for getting 
+    # Total value based on Instance
     def get_total(self, tenant_id=None, begin=None, end=None,
-                  service=None, all_tenants=False):
+                  service=None, instance_id=None, volume_type_id=None, all_tenants=False):
         url = self.base_url + "/total"
         filters = list()
         if tenant_id:
@@ -52,9 +63,90 @@ class ReportManager(base.CrudManager):
             filters.append("service=%s" % service)
         if all_tenants:
             filters.append("all_tenants=%s" % all_tenants)
+        if instance_id:
+            filters.append("instance_id=%s" % instance_id)
+
+        # check for volume type id
+        if volume_type_id:
+           filters.append("volume_type_id=%s" % volume_type_id)
+
         if filters:
             url += "?%s" % ('&'.join(filters))
         return self.client.get(url).json()
+
+
+    # Get invoice based on tenant-id, payment_status and invoice-id args
+    def get_invoice(self, tenant_id=None, invoice_id=None, payment_status=None):
+        url = self.base_url + "/invoice"
+        filters = list()
+        if tenant_id:
+            filters.append("tenant_id=%s" % tenant_id)
+        if invoice_id:
+            filters.append("invoice_id=%s" % invoice_id)
+        if payment_status:
+            filters.append("payment_status=%s" % payment_status)
+        if filters:
+            url += "?%s" % ('&'.join(filters))
+        return [self.resource_class(self, j, loaded=True)
+                for j in self.client.get(url).json()]
+
+
+    # List the invoices, can accept all-tenants arg
+    def list_invoice(self, all_tenants=None):
+        url = self.base_url + "/list_invoice"
+        filters = list()
+        if all_tenants:
+            filters.append("all_tenants=%s" % all_tenants)
+        if filters:
+            url += "?%s" % ('&'.join(filters))
+        return [self.resource_class(self, j, loaded=True)
+                for j in self.client.get(url).json()]
+
+
+    # Show the invoice details
+    def show_invoice(self, invoice_id):
+        url = self.base_url + "/show_invoice"
+        filters = list()
+        if invoice_id:
+            filters.append("invoice_id=%s" % invoice_id)
+        if filters:
+            url += "?%s" % ('&'.join(filters))
+        return [self.resource_class(self, j, loaded=True)
+                for j in self.client.get(url).json()]
+
+
+    # Adding the invoice details, Arguments specified in kwargs 
+    def add_invoice(self, **kwargs):
+        url = self.base_url + "/add_invoice"
+        return self.client.post(url, data = kwargs).json()
+
+    # Updating a invoice
+    def update_invoice(self, **kwargs):
+        url = self.base_url + "/update_invoice"
+        filters = list()
+        if kwargs.get('invoice_id'):
+            filters.append("invoice_id=%s" % kwargs.get('invoice_id'))
+        if kwargs.get('total_cost'):
+            filters.append("total_cost=%s" % kwargs.get('total_cost'))
+        if kwargs.get('paid_cost'):
+            filters.append("paid_cost=%s" % kwargs.get('paid_cost'))
+        if kwargs.get('balance_cost'):
+            filters.append("balance_cost=%s" % kwargs.get('balance_cost'))
+        if kwargs.get('payment_status'):
+            filters.append("payment_status=%s" % kwargs.get('payment_status'))
+        if filters:
+            url += "?%s" % ('&'.join(filters))
+        return self.client.put(url).json()
+
+    # deleting a invoice
+    def delete_invoice(self, **kwargs):
+        url = self.base_url + "/delete_invoice"
+        filters = list()
+        if kwargs.get('invoice_id'):
+            filters.append("invoice_id=%s" % kwargs.get('invoice_id'))
+        if filters:
+            url += "?%s" % ('&'.join(filters))
+        return self.client.delete(url).json()
 
 
 class ReportSummaryManager(ReportManager):
